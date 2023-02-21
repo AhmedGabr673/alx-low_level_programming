df1787d2cf38601fc0e882b8c384ae258bed2ad3
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-22 01:00:31 +0200

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+while (i <=9)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+i++;
+}
+}
+
