34678c29b6b5d2e74a6a5c6616d24639458cdae2
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-22 01:16:47 +0200

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,19 @@
  *
  * Return: Always 0.
  */
+
 void print_alphabet_x10(void)
 {
-int i = 0;
-while (i <= 9)
-{
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
-i++;
-}
+	char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
 }
 
