62c16dee243d7151f23589c2789bfd7b58069a58
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-22 18:05:23 +0200

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,32 +6,22 @@
 
 void jack_bauer(void)
 {
-int min = 0;
-int hours = 0;
-while (hours < 24)
-{
-while (min < 60)
-{
-if (hours < 10)
-{
-_putchar ('0' + hours);
-}
-else
-{
-_putchar (hours);    
-}
-_putchar(':');
-if (min < 10)
-{
-_putchar ('0' + min);
-}
-else
-{
-_putchar (min);    
-}
-min++;
-}
-}
-hours++;
-}
+	int hours = 0;
+	int min = 0;
 
+	while (hours < 24)
+	{
+		while (min < 60)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		min = 0;
+		hours++;
+	}
+}
