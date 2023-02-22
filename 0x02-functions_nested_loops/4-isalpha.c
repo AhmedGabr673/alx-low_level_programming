7de61f52ac89940d98d53b984d2fa101c66f92ec
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-22 15:11:00 +0200

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: character to check
+ * Return: 1 if lowercase or uppercase, 0 if otherwise
+ */
+
+int _islower(int c)
+{
+if ((c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+return (0);
+}
