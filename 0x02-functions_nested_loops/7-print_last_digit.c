770e7484a1f71542c1412ebfec1106ea39cfb3ac
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-22 16:49:19 +0200

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks for lowercase character
+ * @x: int to check
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int x)
+{
+return (x%10);
+}
