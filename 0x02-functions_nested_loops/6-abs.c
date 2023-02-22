9e41133f540ab8837081edfa8d323a569828763e
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-22 16:45:36 +0200

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @x: int to check
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+
+	return (0);
+}
+
