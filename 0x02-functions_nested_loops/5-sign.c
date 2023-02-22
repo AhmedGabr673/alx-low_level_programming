3451e34211d1bdb74ddbfb1b3dae8e249b876a01
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-22 16:08:23 +0200

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,26 @@
 #include "main.h"
-
 /**
- * print_sign - checks for lowercase character
+ * print_sign - checks sign
  * @n: number to check
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ * Return: 1 and prints + if n is greater than zero
+ * Return: 0 and prints 0 if n is zero
+ * Return: -1 and prints - if n is less than zero
  */
-
 int print_sign(int n)
 {
 if (n > 0)
 {
 _putchar('+');
 return (1);    
-}else if (n < 0)
+}
+else if (n < 0)
 {
 _putchar('-');     
 return (-1);
-}else
+}
+else
 {
+_putchar('0');  
 return (0);
 }
 }
