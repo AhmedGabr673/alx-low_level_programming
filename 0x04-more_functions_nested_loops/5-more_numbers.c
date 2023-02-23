dd723e9911427645ed82e38ba79e8d5f5fb4eded
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-23 16:05:32 +0200

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 to 9 except 2 & 4
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int n = 0;
+int count = 0;
+for (i = 0; i < 14; i++)
+{
+for (n = '0'; n <= '14'; n++)
+{
+_putchar (n);
+}
+_putchar ('\n');
+}
+}
