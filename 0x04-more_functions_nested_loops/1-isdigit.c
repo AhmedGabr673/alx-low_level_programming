9b1e52e24f689c57d86227d5e128856d044a722d
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-23 15:29:10 +0200

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if digit
+ * @c: int to check
+ * Return: return 1 if digit
+ */
+
+int _isdigit(int c)
+{
+if (c <= 9)
+{
+    return (1);
+}
+else
+{
+    return (0);
+}
+}
