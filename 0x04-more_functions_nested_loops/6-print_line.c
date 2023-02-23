418614e6d7c46b3e545e7646f7091b186134f274
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-23 16:30:15 +0200

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * @n: line length
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int i = 0;
+for (i = 0; i <= n; i++)
+{
+_putchar ('n');
+}
+_putchar ('\n');
+}
