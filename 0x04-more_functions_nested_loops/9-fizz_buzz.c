6c094d2303f89cb53c4396b41c52112ee781fa69
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-23 18:20:21 +0200

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,15 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0)
 {
 printf("Fizz ");
-continue;
 }
 else if( i % 5 == 0)
 {
 printf("Buzz ");
-continue;
 }
+else
+{
 printf("%d ", i);
+}
 }
 printf("\n");
 return (0);
