7212dfaa8604819eb6313ec23511dd4913291b56
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 21:44:49 +0200

    commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,22 @@
 #include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
 
 /**
  * puts2 - print every other char of a string
@@ -7,13 +25,8 @@
 
 void puts2(char *str)
 {
-int len = 0;
-_putchar(*str);
-while (*str != '\0')
-{
-len++;
-str+=2;
-_putchar(*str);
-}
+int i;
+for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+_putchar(str[i]);
 _putchar('\n');
 }
