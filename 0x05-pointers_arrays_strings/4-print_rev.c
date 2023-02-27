415d5b2c866707d5b6c3d00c44fde98940cf5a3c
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 16:22:20 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev -  function that prints a string, followed by a new line.
+ * @s: pointer to a string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+
+int len = 0;
+
+while (*str != '\0')
+{
+len++
+s++;
+}
+int i = 0;
+for (i = 0; i < len; i++)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
