2d75adf52318a867b8301178665f0a8737b57342
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 15:31:37 +0200

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - pointer to an int as parameter 
+ * and updates the value it points to to 98
  * @n : is address
  * Return: void
  */
