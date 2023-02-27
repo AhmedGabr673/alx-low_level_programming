d8e5fb1d2f3bfc6e36bf004ebe4b2e89563f9e1e
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 21:11:00 +0200

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,72 @@
+// #include "main.h"
+
+// /**
+//  * rev_string -  function that prints a string, followed by a new line.
+//  * @s: pointer to a string
+//  * Return: void
+//  */
+// void rev_string(char *s)
+// {
+
+// int len = 0;
+// int i = 0;
+// int j = 0;
+// char temp;
+
+// while (*s != '\0')
+// {
+// len++;
+// s++;
+// }
+
+// j=len-1;
+// while (i < j)
+// {
+// temp = s[i];
+// s[i] = s[j];
+// s[j] = temp;
+// i++;
+// j--;
+// }
+// }
+
 #include "main.h"
 
 /**
- * rev_string -  function that prints a string, followed by a new line.
- * @s: pointer to a string
- * Return: void
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+
 void rev_string(char *s)
 {
+	int i = 0;
+	int j = (_strlen(s) - 1);
+	char tmp;
 
-int len = 0;
-int i = 0;
-int j = 0;
-char temp;
-
-while (*s != '\0')
-{
-len++;
-s++;
-}
-
-j=len-1;
-while (i < j)
-{
-temp = s[i];
-s[i] = s[j];
-s[j]= temp;
-i++;
-j--;
-}
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++, j--;
+	}
 }
