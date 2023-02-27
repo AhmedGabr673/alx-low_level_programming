914a5ef79456c140b495d197c7754488961265aa
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 16:16:30 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts -  function that prints a string, followed by a new line.
+ * @s: pointer to a string
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+while (*s != '\0')
+{
+_putchar(*s);
+s++;
+}
+_putchar('\n');
+}
