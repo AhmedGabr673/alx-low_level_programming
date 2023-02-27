7e09ac45644e80b71dc03f7ca0735a82dda8e631
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 15:37:27 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a : is address
+ * @b : is address
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int *temp = a;
+a = b;
+b = temp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,7 @@
 #define _main_h
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b)
+
 
 #endif
