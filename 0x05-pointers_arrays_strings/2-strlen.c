63c94c0152625a5810f2a65cf40b4806dbefc715
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 16:13:08 +0200

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen -  function that returns the length of a string.
- * @c: pointer to a string
+ * @s: pointer to a string
  * Return: returns the length of a string.
  */
 int _strlen(char *s)
