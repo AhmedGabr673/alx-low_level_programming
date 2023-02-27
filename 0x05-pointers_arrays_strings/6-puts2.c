85f08fa7964147b8b4c92268f2bafa1d11a91008
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 21:32:53 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other char of a string
+ * @str: string to reverse
+ */
+
+void puts2(char *str)
+{
+
+while (*s != '\0')
+	{
+		len++;
+		s+=2;
+        _putchar(*s);
+	}
+     _putchar('\n');
+}
