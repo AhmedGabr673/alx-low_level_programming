7c28cccc1a3807c3d9b90f04291f60bd12a6562f
AhmedGabr673 <124810947+AhmedGabr673@users.noreply.github.com>
2023-02-27 21:11:40 +0200

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,34 +1,4 @@
-// #include "main.h"
 
-// /**
-//  * rev_string -  function that prints a string, followed by a new line.
-//  * @s: pointer to a string
-//  * Return: void
-//  */
-// void rev_string(char *s)
-// {
-
-// int len = 0;
-// int i = 0;
-// int j = 0;
-// char temp;
-
-// while (*s != '\0')
-// {
-// len++;
-// s++;
-// }
-
-// j=len-1;
-// while (i < j)
-// {
-// temp = s[i];
-// s[i] = s[j];
-// s[j] = temp;
-// i++;
-// j--;
-// }
-// }
 
 #include "main.h"
 
